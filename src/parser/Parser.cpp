d39bea76eb5cf852c83611ca894cfb498c1b71f3
nefimov <n.a.efimov@gmail.com>
2026-05-22 19:09:15 +0200

    add check for reading a directory

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -6,11 +6,13 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 16:45:56 by nefimov           #+#    #+#             */
-/*   Updated: 2026/05/22 16:45:57 by nefimov          ###   ########.fr       */
+/*   Updated: 2026/05/22 19:07:24 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser/Parser.hpp"
+
+#include <sys/stat.h>
 
 #include <fstream>
 #include <sstream>
@@ -20,6 +22,12 @@
 #include "parser/ParserImpl.hpp"
 
 std::vector<ServerConfig> Parser::parseFile(const std::string& path) {
+    struct stat st;
+    if (stat(path.c_str(), &st) == 0 && S_ISDIR(st.st_mode)) {
+        LOG_ERROR("Config path is a directory: " + path);
+        throw std::runtime_error("Config path is a directory: " + path);
+    }
+
     std::ifstream input(path.c_str());
     if (!input) {
         LOG_ERROR("Failed to open config file: " + path);
