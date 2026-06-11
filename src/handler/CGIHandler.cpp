3f70e63e562a408bcad12aeef771a941c2942a6d
tim-daria <darya.timofeeva.97@gmail.com>
2026-06-11 21:31:21 +0200

    fix: take port from config for env as suggested in previous PR

--- src/handler/CGIHandler.cpp
+++ src/handler/CGIHandler.cpp
@@ -37,7 +37,7 @@ std::vector<std::string> CGIHandler::buildEnvironment(const HttpRequest& request
     env.push_back("PATH_INFO=");
     env.push_back("CONTENT_LENGTH=" + toString(request.getBody().size()));
     env.push_back("SERVER_NAME=" + _config.serverName);
-    env.push_back("SERVER_PORT=8080");
+    env.push_back("SERVER_PORT=" + toString(_config.listen[0].second));
     env.push_back("GATEWAY_INTERFACE=CGI/1.1");
     env.push_back("QUERY_STRING=" + request.getQuery());
     env.push_back("SERVER_PROTOCOL=HTTP/1.0");
