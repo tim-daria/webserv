ec94433febedaafdfc9386b5acd3880f31a3329c
tim-daria <darya.timofeeva.97@gmail.com>
2026-06-12 15:27:20 +0200

    fix: change cgi scripts

--- www/webserv/cgi-bin/easy_script.py
+++ www/webserv/cgi-bin/easy_script.py
@@ -9,18 +9,14 @@ if os.environ.get("REQUEST_METHOD") == "POST":
     body = sys.stdin.read()
     print("Content-Type: text/plain")
     print()
-    print("=== CGI TEST (POST) ===")
     print("Body:", body)
 if os.environ.get("REQUEST_METHOD") == "GET":
 	print("Content-Type: text/plain")
 	print()
-
-	print("=== CGI TEST ===")
 	print("Method:", os.environ.get("REQUEST_METHOD"))
-	print("Path:", os.environ.get("PATH_INFO"))
-	print("Query:", os.environ.get("QUERY_STRING"))
-    # Get current working directory
-	current_directory = os.getcwd()
+	query = os.environ.get("QUERY_STRING", "")
 
-	# Print it
-	print("Current working directory:", current_directory)
+	if not query:
+		print("Hello from CGI!")
+	else:
+		print("Hello", query)
